2b744352d62e5b2c9f28089479b44d9ca3d506ab
ShaunCoutts <shaun.coutts@gmail.com>
2015-10-02 13:48:17 +0100

    even simpler model of herb resistance
    Just NTSR resistance, no space and annual time step

---
+++ BG_model/herb_resist_nonspace.R
@@ -0,0 +1,1 @@
+#Simple model of evolution of herbicide resistance as a quantitative trait. Non-spatial, annual timestep with only non-target site resistance.  
---
+++ BG_model/herb_resist_proccess_functions.R
@@ -0,0 +1,3 @@
+#set of functions for herbicide resitance model covering various processes like genotype production and seed bank dynamics
+
+##
